db9e15854470fd086318e369f6cdef5422ac9fc5
PeanutNotCashew <ellafan227@gmail.com>
2026-01-31 20:41:38 +0000

    Worker draft (DOES NOT WORK)

--- greyteam/roles/scoring_server/files/worker.py
+++ greyteam/roles/scoring_server/files/worker.py
@@ -13,6 +13,18 @@ from server import (
     periodic_ansible, setup_logging, create_db_tables, start_server
 )
 
+'''
+Used by threads to output check results to the main thread
+
+@param check_func the function which carries out the check
+@param args arguments to put into the check
+@param service_id the ID of the service associated with the check
+@param out the dictionary which takes the thread output. The key is service ID and the value is the check output (successful/nonsuccessful)
+'''
+def check(check_func:Callable, args:list, service_id:int, out:dict[int, int]):
+    out[check_name] = -1
+    out[check_name] = check_func(args)
+
 if __name__ == "__main__":
     logger = setup_logging()
     logger.info("Starting background worker threads...")
@@ -36,6 +48,52 @@ if __name__ == "__main__":
     
     notifier.notify("READY=1")
     logger.info("READY signal sent to systemd.")
+
+    # Pull services from db
+    try:
+        logger.info("Pulling services")
+        services = Service.query.all()
+    except Exception as e:
+        logger.error("Failed to pull services - Exiting...")
+        return
+
+    # Perform checks
+    round_num:int = 1
+    while True:
+        logger.info(f'Beginning to score round {round_num}')
+
+        res : dict[int, int] = {} # Relate service ID to result
+        # Get check info
+        for service in services:
+            # Load in scoring criteria
+            session.query(ScoringCriteria).\
+                filter(ScoringCriteria.service_id == service.id)
+            # Match to score type (HTTP, etc)
+            # Add new thread to checks dict, with value being -1
+
+        # Start checks
+        for check in checks:
+            check.start()
+
+        # Wait for checks
+        time.sleep(60)
+
+        # Update to service history
+        for service in services:
+            newScore = ScoringHistory(
+                service_id = service.id,
+                host_id = service.host_id,
+                round = round_num,
+                value = res[services.id]
+            )
+            db.session.add(new_score)
+            logger.info(f"Created round {round_num} for service {service_id}")
+            db.session.commit()
+
+        # Increment round number
+        round_num ++
+
+            
 
     # Keep the main process alive
     try:
