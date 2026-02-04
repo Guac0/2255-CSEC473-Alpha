ff698288ef07659ac9c188d259c950f69accce93
Aknee <annyhua04@gmail.com>
2026-02-04 02:14:32 -0500

    changed comment

--- greyteam/terraform/host_cement.tf
+++ greyteam/terraform/host_cement.tf
@@ -6,7 +6,7 @@ resource "openstack_compute_instance_v2" "fillydelphia" {
   key_pair        = "cdt"
 
   block_device {
-    uuid                  = "1cde3ec3-03d3-4c17-a512-d3799ae92dad" #ubuntu2404desktop need to change to chachyos
+    uuid                  = "1cde3ec3-03d3-4c17-a512-d3799ae92dad" #ubuntu2404desktop need to change to cement
     source_type           = "image"
     destination_type      = "volume"
     volume_size           = 60
