ae72299ad9b0e185be098db66247764854546795
Guac0 <YeetYeetGuac@gmail.com>
2026-02-04 22:25:43 -0500

    setup.sh - correct path

--- greyteam/setup.sh
+++ greyteam/setup.sh
@@ -1,7 +1,7 @@
 apt update
 apt install -y nano git python-is-python3 python3 python3-venv python3-pip sshpass pwgen
 git clone https://github.com/Guac0/2255-CSEC473-Alpha/
-curl https://download.documentfoundation.org/libreoffice/stable/26.2.0/win/x86_64/LibreOffice_26.2.0_Win_x86-64.msi > 2255-CSEC473-Alpha/greyteam/roles/windows_mssql/files/LibreOffice_Win_x86-64.msi
+curl https://download.documentfoundation.org/libreoffice/stable/26.2.0/win/x86_64/LibreOffice_26.2.0_Win_x86-64.msi > 2255-CSEC473-Alpha/greyteam/roles/windows_utilities/files/LibreOffice_Win_x86-64.msi
 chown greyteam:greyteam -R 2255-CSEC473-Alpha
 cd ./2255-CSEC473-Alpha/greyteam
 python -m venv venv
