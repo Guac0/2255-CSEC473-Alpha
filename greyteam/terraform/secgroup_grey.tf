d675a934140e9b8aadc4e46a92529a21a64fa386
Aknee <annyhua04@gmail.com>
2026-02-13 15:02:27 -0500

    dupe

--- greyteam/terraform/secgroup_grey.tf
+++ greyteam/terraform/secgroup_grey.tf
@@ -59,16 +59,16 @@ resource "openstack_networking_secgroup_rule_v2" "grey_ingress_444" {
 # }
 
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_grey_rule_3" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
-  #remote_ip_prefix  = "0.0.0.0/0"
-  #remote_group_id   = ""
-  security_group_id = openstack_networking_secgroup_v2.secgroup_grey.id
-}
+# resource "openstack_networking_secgroup_rule_v2" "secgroup_grey_rule_3" {
+#   direction         = "egress"
+#   ethertype         = "IPv4"
+#   #protocol          = ""
+#   #port_range_min    = 0
+#   #port_range_max    = 0
+#   #remote_ip_prefix  = "0.0.0.0/0"
+#   #remote_group_id   = ""
+#   security_group_id = openstack_networking_secgroup_v2.secgroup_grey.id
+# }
 
 # resource "openstack_networking_secgroup_rule_v2" "secgroup_grey_rule_4" {
 #   direction         = "egress"
