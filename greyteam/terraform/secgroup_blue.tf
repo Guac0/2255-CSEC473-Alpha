8c6a6c24235addb92909d9d75ade1e0e3e02eabb
Aknee <annyhua04@gmail.com>
2026-02-13 14:54:24 -0500

    rules

--- greyteam/terraform/secgroup_blue.tf
+++ greyteam/terraform/secgroup_blue.tf
@@ -1,65 +1,48 @@
 resource "openstack_networking_secgroup_v2" "secgroup_blue" {
-  name        = "secgroup_blue"
+  name = "secgroup_blue"
 }
 
-# resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_1" {
-#   direction         = "ingress"
-#   ethertype         = "IPv4"
-#   #protocol          = ""
-#   #port_range_min    = 0
-#   #port_range_max    = 0
-#   #remote_ip_prefix  = "0.0.0.0/0"
-#   #remote_group_id   = ""
-#   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
-# }
+##########
+# Internal Blue Communication
+##########
 
-# resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_2" {
-#   direction         = "ingress"
-#   ethertype         = "IPv6"
-#   #protocol          = ""
-#   #port_range_min    = 0
-#   #port_range_max    = 0
-#   #remote_ip_prefix  = "0.0.0.0/0"
-#   #remote_group_id   = ""
-#   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
-# }
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_0" {
+resource "openstack_networking_secgroup_rule_v2" "blue_internal_ingress" {
   direction         = "ingress"
   ethertype         = "IPv4"
   remote_ip_prefix  = "172.20.0.32/27"
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
-#innbound from inscope
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_1" {
+
+############################################
+# Inbound from Inscope (Core/Internal/DMZ)
+############################################
+
+resource "openstack_networking_secgroup_rule_v2" "blue_ingress_core" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
   remote_ip_prefix  = "10.0.10.0/24"
-  #remote_group_id   = ""
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_2" {
+resource "openstack_networking_secgroup_rule_v2" "blue_ingress_internal" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
   remote_ip_prefix  = "10.0.20.0/24"
-  #remote_group_id   = ""
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_3" {
+
+resource "openstack_networking_secgroup_rule_v2" "blue_ingress_dmz" {
   direction         = "ingress"
   ethertype         = "IPv4"
   remote_ip_prefix  = "10.0.30.0/24"
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
-#SSH from WAN/NAT
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_4" {
+
+############################################
+# SSH from NAT/WAN ONLY
+############################################
+
+resource "openstack_networking_secgroup_rule_v2" "blue_ingress_ssh_nat" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -69,42 +52,47 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_4" {
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
+############################################
+# Outbound to Inscope
+############################################
 
-
-#outbund from inscope
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_5" {
+resource "openstack_networking_secgroup_rule_v2" "blue_egress_core" {
   direction         = "egress"
   ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
   remote_ip_prefix  = "10.0.10.0/24"
-  #remote_group_id   = ""
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_6" {
+resource "openstack_networking_secgroup_rule_v2" "blue_egress_internal" {
   direction         = "egress"
   ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
   remote_ip_prefix  = "10.0.20.0/24"
-  #remote_group_id   = ""
-  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
-}
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_7" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
-  remote_ip_prefix  = "10.0.30.0/24"
-  #remote_group_id   = ""
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_8" {
+resource "openstack_networking_secgroup_rule_v2" "blue_egress_dmz" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "10.0.30.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+############################################
+# Outbound to Grey (General)
+############################################
+
+resource "openstack_networking_secgroup_rule_v2" "blue_egress_grey" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "172.20.0.64/26"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+############################################
+# Scoring Server Access (443 & 444)
+############################################
+
+resource "openstack_networking_secgroup_rule_v2" "blue_scoring_443" {
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -114,21 +102,7 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_8" {
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_9" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  remote_ip_prefix  = "172.20.0.64/26"
-  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_10" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  remote_ip_prefix  = "100.65.0.0/16"
-  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_11" {
+resource "openstack_networking_secgroup_rule_v2" "blue_scoring_444" {
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -137,26 +111,112 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_11" {
   remote_ip_prefix  = "172.20.0.100/32"
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
-/*
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_3" {
+
+############################################
+# Outbound to NAT/WAN
+############################################
+
+resource "openstack_networking_secgroup_rule_v2" "blue_egress_nat" {
   direction         = "egress"
   ethertype         = "IPv4"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
-  #remote_ip_prefix  = "0.0.0.0/0"
-  #remote_group_id   = ""
+  remote_ip_prefix  = "100.65.0.0/16"
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_blue_rule_4" {
-  direction         = "egress"
-  ethertype         = "IPv6"
-  #protocol          = ""
-  #port_range_min    = 0
-  #port_range_max    = 0
-  #remote_ip_prefix  = "0.0.0.0/0"
-  #remote_group_id   = ""
+#############
+# SSH 22 rules
+#############
+
+# TCP 1-21
+resource "openstack_networking_secgroup_rule_v2" "blue_core_tcp_low" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 21
+  remote_ip_prefix  = "10.0.10.0/24"
   security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
 }
-*/
+
+# TCP 23-65535
+resource "openstack_networking_secgroup_rule_v2" "blue_core_tcp_high" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 23
+  port_range_max    = 65535
+  remote_ip_prefix  = "10.0.10.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+resource "openstack_networking_secgroup_rule_v2" "blue_internal_tcp_low" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 21
+  remote_ip_prefix  = "10.0.20.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "blue_internal_tcp_high" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 23
+  port_range_max    = 65535
+  remote_ip_prefix  = "10.0.20.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+resource "openstack_networking_secgroup_rule_v2" "blue_dmz_tcp_low" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 21
+  remote_ip_prefix  = "10.0.30.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "blue_dmz_tcp_high" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 23
+  port_range_max    = 65535
+  remote_ip_prefix  = "10.0.30.0/24"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+resource "openstack_networking_secgroup_rule_v2" "blue_inscope_udp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "blue_inscope_icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+resource "openstack_networking_secgroup_rule_v2" "blue_inscope_udp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "blue_inscope_icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "10.0.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_blue.id
+}
