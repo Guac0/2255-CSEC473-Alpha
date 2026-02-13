b72341dcda24565f085833ef9087e7c8a5f1080b
Aknee <annyhua04@gmail.com>
2026-02-13 16:38:56 -0500

    red

--- greyteam/terraform/secgroup_red.tf
+++ greyteam/terraform/secgroup_red.tf
@@ -1,3 +1,7 @@
+resource "openstack_networking_secgroup_v2" "secgroup_red" {
+  name        = "secgroup_red"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "secgroup_red_rule_1" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -17,7 +21,7 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_red_rule_2" {
   #port_range_max    = 0
   #remote_ip_prefix  = "0.0.0.0/0"
   #remote_group_id   = ""
-  security_group_id = openstack_networking_secgroup_v2.secgroup_red.id
+  security_group_id = openstack_networking_secgroup_v2.secgroup_red.id 
 }
 
 /*
