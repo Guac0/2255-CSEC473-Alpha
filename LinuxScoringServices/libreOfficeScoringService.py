3ec852ff4037c1ac76e457bd9d635aded40d9844
dariusF <181darius@gmail.com>
2026-02-17 13:37:30 -0500

    checking desktop stuff

--- LinuxScoringServices/libreOfficeScoringService.py
+++ LinuxScoringServices/libreOfficeScoringService.py
@@ -1,27 +1,39 @@
 import subprocess
+import socket
 
-HOSTS = ["10.0.30.4", "10.0.30.5", "10.0.30.6"] # cloudsdale", "vanhoover", "whinnyapolis"
+#HOSTS = ["10.0.30.4", "10.0.30.5", "10.0.30.6"] # cloudsdale", "vanhoover", "whinnyapolis"
 USER  = "twilight"
 CMD   = "timeout 10s libreoffice --headless --version"
 
-any_workstation_failed  = False
 
-for host in HOSTS:
-    try:
-        ssh_command_result  = subprocess.run(
-            ["ssh", f"{USER}@{host}", CMD],
-            capture_output=True,
-            text=True,
-            timeout=15
-        )
 
-        
-        combined_output  = (ssh_command_result.stdout + ssh_command_result.stderr).strip()
-        if ssh_command_result.returncode != 0 or "LibreOffice" not in combined_output:
-            any_workstation_failed  = True
+HOST = '10.0.30.4'
+PORT = 12345
 
-    except Exception:
-        print("DOWN")
-        raise SystemExit(2)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+data = client.recv(1024)
+if data.decode() == "Python 3.13.7\n": print("YAY")
+client.close()
 
-print("BAD" if any_workstation_failed  else "OK")
+#any_workstation_failed  = False
+#
+#for host in HOSTS:
+#    try:
+#        ssh_command_result  = subprocess.run(
+#            ["ssh", f"{USER}@{host}", CMD],
+#            capture_output=True,
+#            text=True,
+#            timeout=15
+#        )
+#
+#        
+#        combined_output  = (ssh_command_result.stdout + ssh_command_result.stderr).strip()
+#        if ssh_command_result.returncode != 0 or "LibreOffice" not in combined_output:
+#            any_workstation_failed  = True
+#
+#    except Exception:
+#        print("DOWN")
+#        raise SystemExit(2)
+#
+#print("BAD" if any_workstation_failed  else "OK")
