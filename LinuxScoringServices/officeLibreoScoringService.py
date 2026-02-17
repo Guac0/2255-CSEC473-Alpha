6eeffaac1429dcf4bc0f994386a8f8601b0f85c7
dariusF <181darius@gmail.com>
2026-02-16 21:03:18 -0500

    finishing scoring script

--- LinuxScoringServices/officeLibreoScoringService.py
+++ LinuxScoringServices/officeLibreoScoringService.py
@@ -1,21 +1,27 @@
 import subprocess
 
-HOST = "cloudsdale"      
-USER = "twilight"         
-CMD  = "timeout 10s libreoffice --headless --version"
+HOSTS = ["10.0.30.4", "10.0.30.5", "10.0.30.6"] # cloudsdale", "vanhoover", "whinnyapolis"
+USER  = "twilight"
+CMD   = "timeout 10s libreoffice --headless --version"
 
-try:
-    r = subprocess.run(
-        ["ssh", f"{USER}@{HOST}", CMD],
-        capture_output=True,
-        text=True,
-        timeout=15
-    )
+any_workstation_failed  = False
 
-    if r.returncode == 0 and r.stdout.strip():
-        print("OK")
-    else:
-        print("BAD")
+for host in HOSTS:
+    try:
+        ssh_command_result  = subprocess.run(
+            ["ssh", f"{USER}@{host}", CMD],
+            capture_output=True,
+            text=True,
+            timeout=15
+        )
 
-except Exception:
-    print("DOWN")
+        
+        combined_output  = (ssh_command_result.stdout + ssh_command_result.stderr).strip()
+        if ssh_command_result.returncode != 0 or "LibreOffice" not in combined_output:
+            any_workstation_failed  = True
+
+    except Exception:
+        print("DOWN")
+        raise SystemExit(2)
+
+print("BAD" if any_workstation_failed  else "OK")
