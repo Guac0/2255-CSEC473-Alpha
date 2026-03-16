6226c7ae5524ff0d9bdb13993b69259fcb0e2465
Guac0 <YeetYeetGuac@gmail.com>
2026-03-16 17:31:36 -0400

    update playbook

--- blueteam/team2/setup.sh
+++ blueteam/team2/setup.sh
@@ -1,6 +1,6 @@
 echo "This script assumes that you are running on a Debian-based OS, have a blueteam user, and are currently running as root."
 apt update
-apt install -y nano git curl python-is-python3 python3 python3-venv python3-pip sshpass pwgen
+apt install -y nano git curl python-is-python3 python3 python3-venv python3-pip sshpass pwgen ansible
 git clone https://github.com/Guac0/2255-CSEC473-Alpha/
 chown blueteam:blueteam -R 2255-CSEC473-Alpha
 cd ./2255-CSEC473-Alpha/blueteam/team2/ansible
