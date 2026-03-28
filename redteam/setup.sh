b59e8525ed63a5e0db54a44b5cb648107a27216e
Guac0 <YeetYeetGuac@gmail.com>
2026-03-28 18:00:07 -0400

    redteam - add initial ansible using excerpts from blue/grey, add inventory files for each environment, add initial work on GOST proxy deploy

---
+++ redteam/setup.sh
@@ -0,0 +1,12 @@
+echo "This script assumes that you are running on a Debian-based OS, have a redteam user, and are currently running as root."
+apt update
+apt install -y nano git curl python-is-python3 python3 python3-venv python3-pip sshpass pwgen ansible
+git clone https://github.com/Guac0/2255-CSEC473-Alpha/
+chown redteam:redteam -R 2255-CSEC473-Alpha
+cd ./2255-CSEC473-Alpha/redteam/ansible
+python -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+activate-global-python-argcomplete
+ansible-galaxy install -r requirements.yml
+echo "Finished. Ansible and venv set up at ./2255-CSEC473-Alpha/redteam/ansible. Make sure to `source venv/bin/activate` before running Ansible tasks."
